a144fd4c5cb9258473e6680306e0fd9dbb61db4b
Evan Turnbull <evanturn@gmail.com>
2017-05-29 19:43:55 +1000

    Cleaned up ContactUs. Now using the backend.

--- src/app/contactus/contactus.component.ts
+++ src/app/contactus/contactus.component.ts
@@ -1,42 +1,44 @@
-import { Component, ElementRef } from "@angular/core";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { Component, ElementRef } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
-import {ContactService} from "./services/contact.service";
-import {Contact} from "./models/contact";
-import {AppConsts} from "../app.consts";
+import {ContactService} from './services/contact.service';
+import {Contact} from './models/contact';
+import {AppConsts} from '../app.consts';
 
 declare var jQuery: any;
 
 function emailValidator(control: FormControl): { [s: string]: boolean } {
 
-    let pattern =  /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const pattern =  /^(([^<>()[\]\\.,;:\s@\']+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     //
     return control.value.match(pattern) ? null : {pattern: true};
 }
 
 @Component({
-  selector: "contact",
+  selector: 'app-contact',
   providers: [ContactService],
-  templateUrl: "contactus.component.html",
-  styleUrls: ["contactus.component.css"]
+  templateUrl: 'contactus.component.html',
+  styleUrls: ['contactus.component.css']
 })
 export class ContactUsComponent {
 
-  success: boolean = false;
+  success = false;
   errorMessage: string;
+  // used to control the spinny thing when waiting for a response
+  loading = false;
 
   appConsts: AppConsts;
 
   // Just trying to bind to a model class - main reason is to allow better diagnostics
   // and hopefully sort out the problem where the input field doesn"t alway map to the
   // actual form value
-  contact = new Contact("", "", "");
+  contact = new Contact('', '', '');
 
 
   // form controls & group needed to unpin this form
-  name = new FormControl("", Validators.required);
-  email = new FormControl("", Validators.compose([Validators.required, emailValidator]));
-  message = new FormControl("", [Validators.required, Validators.maxLength(300)]);
+  name = new FormControl('', Validators.required);
+  email = new FormControl('', Validators.compose([Validators.required, emailValidator]));
+  message = new FormControl('', [Validators.required, Validators.maxLength(300)]);
 
   contactForm = new FormGroup({
     name: this.name,
@@ -48,22 +50,20 @@ export class ContactUsComponent {
       this.appConsts = _appConsts;
   }
 
-
-
  private successfulSubmit() {
 
-    /*console.log("successful submit");*/
+    this.loading = false;  // turn off the spinny thing
     this.success = true;
 
     // clear out the form controls
-    this.name.reset("");
-    this.email.reset("");
-    this.message.reset("");
+    this.name.reset('');
+    this.email.reset('');
+    this.message.reset('');
 
   }
 
   private failedSubmit(error: any) {
-    /*console.log("failedSubmit called: ", error);*/
+    this.loading = false;  // turn off the spinny thing
     this.success = false;
     this.errorMessage = error;
 
@@ -73,25 +73,22 @@ export class ContactUsComponent {
 
     // clear out my result fields...
     this.success = false;
-    this.errorMessage = "";
+    this.errorMessage = '';
 
     // TODO Might need to double check this in safari as the autocomplete
-    // doesn"t work correctly with angular2
-    let nameVal = jQuery(this.elementRef.nativeElement).find("#name").val();
-    /*console.log("name value read = ", nameVal);
+    // doesn't work correctly with angular2
+    const nameVal = jQuery(this.elementRef.nativeElement).find('#name').val();
 
-    console.log("Name from form control = ", this.name.value);
-*/
-    let contact = new Contact(this.contactForm.controls["name"].value,
-                              this.contactForm.controls["email"].value,
-                              this.contactForm.controls["message"].value);
+    const contact = new Contact(this.contactForm.controls['name'].value,
+                              this.contactForm.controls['email'].value,
+                              this.contactForm.controls['message'].value);
 
+    // toggle the loading flag
+    this.loading = true;
 
     this.contactService.sendMsg( contact )
-                          .subscribe( contact => this.successfulSubmit(),
+                          .subscribe( contactRet => this.successfulSubmit(),
                                       error => this.failedSubmit(<any>error));
-
-
 
 
   }
--- src/app/contactus/services/contact.service.ts
+++ src/app/contactus/services/contact.service.ts
@@ -3,28 +3,23 @@ import {Http, Response, Headers, RequestOptions} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
 
 import {Contact} from '../models/contact';
+import {environment} from '../../../environments/environment';
 
 @Injectable()
 export class ContactService {
+
+  private apiUrl: string = environment.server_url + '/api/v1/contact';
+
 
   constructor(private http: Http) { }
 
   sendMsg(contact: Contact): Observable<Contact> {
 
-    /*console.log("About to send: ", contact);*/
+    const headers = new Headers({'Content-Type': 'application/json'});
+    const options = new RequestOptions({ headers: headers});
+    const body = JSON.stringify( contact );
 
-    let headers = new Headers({'Content-Type': 'application/json'});
-    let options = new RequestOptions({ headers: headers});
-    // TODO - not sure if this will work or I need the name: name format?
-    // Also if I need an observable returned then should I just create an
-    // object to carry the message and any response fields?
-    let body = JSON.stringify( contact );
-    /*console.log("Json stringify result=", body);*/
-    //var targetUrl = new Config().BACKEND_BASE_URL + 'api/v1/contact';
-    var targetUrl = 'api/v1/contact';
-    /*console.log("Sending to targetUrl= ", targetUrl);*/
-
-    return this.http.post(targetUrl, body, options)
+    return this.http.post(this.apiUrl, body, options)
       .timeout(20000)
       .map(this.extractData)
       .catch(this.handleError);
@@ -32,18 +27,15 @@ export class ContactService {
   }
 
   private extractData(res: Response) {
-    if( res.status < 200 || res.status >= 300) {
+    if ( res.status < 200 || res.status >= 300) {
       throw new Error('Response status: ' + res.status);
     }
-    let body = res.json();
+    const body = res.json();
     return body.data || { };
   }
 
   private handleError( error: any) {
-    // TODO consider using a remote logging service to capture this
-    // error and return to us so we know about it
-    /*console.log("Error obj contains: ", error);*/
-    let errMsg = error.message || 'Server error';
+    const errMsg = error.message || 'Server error';
     console.error(errMsg);
     return Observable.throw(errMsg);
   }
