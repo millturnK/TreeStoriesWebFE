6c9cf02ae706edea6f7c970a03569b4518ef34e5
Evan Turnbull <evanturn@gmail.com>
2017-06-18 19:04:47 +1000

    Just commenting out the formData.add links for the time being.

--- src/app/services/story.service.ts
+++ src/app/services/story.service.ts
@@ -48,7 +48,7 @@ export class StoryService {
 
 
     // TODO Not available in story yet
-    formData.append('links', []);
+    // formData.append('links', []);
 
     // formData.append('test', 'test');
     for (const pic of pictures) {
@@ -87,7 +87,7 @@ export class StoryService {
   formData.append('botName', story.botName);
   formData.append('photoLinks', JSON.stringify(story.photoLinks));
   // TODO Not available in story yet
-  formData.append('links', []);
+  // formData.append('links', []);
 
 
   // formData.append('test', 'test');
