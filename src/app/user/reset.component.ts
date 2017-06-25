79c01813d989f6d3ecf6b1a03e8ffc424c227b5e
Evan Turnbull <evanturn@gmail.com>
2017-06-25 10:41:05 +1000

    Change default stories to 5. Also fixed a spam clicking bug in forgot/reset pages. Both picked up by our quality testers!

--- src/app/user/reset.component.ts
+++ src/app/user/reset.component.ts
@@ -1,31 +1,30 @@
-import { Component } from "@angular/core";
-// import {App} from "../app.component";
-import { User } from "./models/user";
-import { FormControl, FormGroup, Validators } from "@angular/forms";
-import { ActivatedRoute, Router } from "@angular/router";
-import { ResetService } from "./services/reset.service";
+import {Component, OnInit} from '@angular/core';
+import { User } from './models/user';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ResetService } from './services/reset.service';
 
 
 function passwordValidator(control: FormControl): { [s: string]: boolean } {
 
-  let pattern = /(?=^.{6,15}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+}{";:"?/>.<,])(?!.*\s).*$/;
+  const pattern = /(?=^.{6,15}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*()_+}{";:"?/>.<,])(?!.*\s).*$/;
   //
   return control.value.match(pattern) ? null : {pattern: true};
 
 }
 
 @Component({
-  selector: "reset-form",
+  selector: 'reset-form',
   providers: [ResetService],
-  templateUrl: "reset.component.html",
-  styleUrls: ["login.components.css"]
+  templateUrl: 'reset.component.html',
+  styleUrls: ['login.components.css']
 
 })
 
-export class ResetComponent {
+export class ResetComponent implements OnInit {
 
-  password = new FormControl("", Validators.compose([Validators.required, passwordValidator]));
-  passwordConfirm = new FormControl("", Validators.compose([Validators.required, passwordValidator]));
+  password = new FormControl('', Validators.compose([Validators.required, passwordValidator]));
+  passwordConfirm = new FormControl('', Validators.compose([Validators.required, passwordValidator]));
 
   resetForm = new FormGroup({
     pwd: this.password,
@@ -33,9 +32,9 @@ export class ResetComponent {
   });
 
   // Use this error message string if the token is dodgy
-  public errorMsg = "";
+  public errorMsg = '';
 
-  success: boolean = false;
+  success = false;
   // Use this error message string is there is any problem validating the passwords or if the reset fails
   errorMessage: string;
 
@@ -43,13 +42,15 @@ export class ResetComponent {
 
   public emailAddr: string;
 
+  // loading flag to toggle when a reset has been requested, so we don't get 'spam clicking'
+  loading = false;
+
   constructor(private route: ActivatedRoute, private _service: ResetService,
               private _router: Router, private _user: User) { }
 
   ngOnInit() {
 
-    this.tokenid = this.route.snapshot.params["id"];
-    /*console.log("Ensure this reset token corresponds to a user: ", this.tokenid);*/
+    this.tokenid = this.route.snapshot.params['id'];
     // kick off user retrieval from back-end
 
     this._service.getUserByToken( this.tokenid )
@@ -59,7 +60,6 @@ export class ResetComponent {
   }
 
   private successfulSearch(user: User) {
-    /*console.log("ok, that token is valid for : ", user.firstname);*/
 
     // clear anything out that is there currently
     this._user.clear();
@@ -71,10 +71,8 @@ export class ResetComponent {
   }
 
   private failedSearch(error: any) {
-    /*console.log("Search failed: ", error);*/
     this.errorMsg = error;
   }
-
 
 
 
@@ -82,16 +80,18 @@ export class ResetComponent {
 
     // clear out my result fields...
     this.success = false;
-    this.errorMessage = "";
+    this.errorMessage = '';
 
     // make sure the passwords are equal
     if (this.password.value !== this.passwordConfirm.value) {
 
-      this.errorMessage = "The passwords you have provided do not match";
+      this.errorMessage = 'The passwords you have provided do not match';
 
       return;
     }
 
+    // disable the button, put in place the spinner
+    this.loading = true;
 
     this.reset();
 
@@ -115,8 +115,8 @@ export class ResetComponent {
 
 
   private successfulReset(user: User) {
-    /*console.log("ok, password reset succeeded : ", user.firstname);*/
     this.success = true;
+    this.loading = false;
 
     // store the important stuff on the global user object
     this._user.firstname = user.firstname;
@@ -130,17 +130,21 @@ export class ResetComponent {
     this.emailAddr = user.username;
     // send them back to the main page, in a few seconds
 
+    // clear out the fields as well
+    this.password.setValue('');
+    this.passwordConfirm.setValue('');
+
     setTimeout(() => { // 3
-      this._router.navigate(["/main"]);
-    }, 4000);
+      this._router.navigate(['/main']);
+    }, 3000);
 
 
   }
 
   private failedReset(error: any) {
-    /*console.log("Reset failed: ", error);*/
-    // this.success = false;
+    this.loading = false;
     this.errorMsg = error;
+
 
   }
 
